3c20dcc6d785f807ef1a43963aa1f32a477ea6b1
shafhad <66770462+shafhad@users.noreply.github.com>
2021-04-24 11:43:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var database, game, form, player;
+var position;
+var gameState, playerCount;
+var allPlayers,gameState=0
+var horse1,horse2,horses=[]
+var hurdle1,hurdle2,hurdle3;
+
+yVel = 0;
+xVel = 0;
+
+function preload(){
+  h1=loadAnimation("images/ho1.0.png","images/ho1.png")
+  h2=loadAnimation("images/ho4.0.png","images/ho4.png")
+  track=loadImage("images/bg1.png")
+  hurdel=loadImage("images/hurdle.png")
+}
+function setup(){
+  database = firebase.database();
+ 
+  createCanvas(displayWidth,displayHeight);
+  game=new Game()
+  game.readGameState()
+  game.startGame()
+
+ }
+
+function draw(){
+ 
+ /* if(keyDown("space")){
+    horse1.velocityX=-12
+    horse2.velocityX=-12
+    console.log(horse1.velocityX)
+}*/
+
+
+
+  if(playerCount===2){
+    game.updateGameState(1)  
+  }
+  if(gameState===1){
+    //clear()
+    game.play()
+  }
+  if(gameState===2){
+  game.end()
+  }
+    //drawSprites();
+  
+}
+
+
